2bb8d29b24e88bdebaaa3e922260fc631cecc87f
mohap710 <mohap710@gmail.com>
2023-01-03 11:27:10 -0800

    fixing pycode style

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,4 @@ for first in range(0, 10):
         if first == 8 & second == 9:
             print("{}{}".format(first, second))
         else:
-            print("{}{}".format(first, second), end=", ")  
-    
+            print("{}{}".format(first, second), end=", ") 

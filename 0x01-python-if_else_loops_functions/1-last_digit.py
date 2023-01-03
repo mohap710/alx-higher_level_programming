298a293d9085eab4012d8a6fcd23654c4001fba7
mohap710 <Mohap710@gmail.com>
2023-01-03 17:51:53 +0300

    Task 2. getting the last digit of int

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = abs(number) % 10
+if last == 0:
+    msg = "and is 0"
+elif last > 5:
+    msg = "and is greater than 5"
+elif 0 < last < 6:    
+    msg = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {last} {msg}")

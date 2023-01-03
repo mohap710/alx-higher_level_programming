a45f5600c0d49f18c7c335cbef50a8440f35bbc9
mohap710 <Mohap710@gmail.com>
2023-01-03 20:53:11 +0300

    Use .format insted of f-string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
-msg = ""
+digit = abs(number) % 10
 if number < 0:
-    last = -last
-if last == 0:
-    msg = "and is 0"
-elif last > 5:
-    msg = "and is greater than 5"
-elif 0 < last < 6:
-    msg = "and is less than 6 and not 0"
-print(f"Last digit of {number:d} is {last:d} {msg}")
+    digit = -digit
+print(f"Last digit of {number:d} is {digit:d} and is ", end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 | i != 113:
-        print(f"{chr(i)}", end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")

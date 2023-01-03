ee01783691346c2d893b801b55add8ad68465cf8
mohap710 <Mohap710@gmail.com>
2023-01-03 18:20:50 +0300

    Pycode style passed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ if last == 0:
     msg = "and is 0"
 elif last > 5:
     msg = "and is greater than 5"
-elif 0 < last < 6:    
+elif 0 < last < 6:
     msg = "and is less than 6 and not 0"
 print(f"Last digit of {number} is {last} {msg}")

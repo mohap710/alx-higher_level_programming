d5b35589a8621bc06b8c4b4c0f7718a791677373
mohap710 <mohap710@gmail.com>
2023-01-08 07:46:50 +0200

    minor fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
     # idx should be in range of the list
     if idx < 0 or idx >= len(my_list):
-        return my_list
-    new_list = my_list.copy()
+        return new_list
+
     new_list[idx] = element
     return new_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)

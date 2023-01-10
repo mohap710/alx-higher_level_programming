68d26470d3e569416188970250523610b6e5c96a
mohap710 <mohap710@gmail.com>
2023-01-10 09:38:32 +0200

    even and odd numbers in list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for el in my_list:
+        new_list.append(
+            True if el % 2 == 0
+            else False
+        )
+    print (new_list)
+    return new_list

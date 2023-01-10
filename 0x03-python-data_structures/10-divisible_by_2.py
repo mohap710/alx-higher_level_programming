b388db79252448707ba21cd627ef9b75e9b959b5
mohap710 <mohap710@gmail.com>
2023-01-10 09:45:06 +0200

    pycodestyle check

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,5 @@ def divisible_by_2(my_list=[]):
     for el in my_list:
         new_list.append(
             True if el % 2 == 0
-            else False
-        )
-    print (new_list)
+            else False)
     return new_list

1a34bc865a2c5b68a8684eaa7c8e130cb84f7210
mohap710 <mohap710@gmail.com>
2023-01-11 13:43:32 +0200

    code optmization

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = []
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                common.append(i)
-                break
-    return common
+    return set(set_1).intersection(set_2)

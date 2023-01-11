e3787aa0b1c8b12bd38e60ef01b0cf712bb83ee0
mohap710 <mohap710@gmail.com>
2023-01-11 18:17:36 +0200

    Code check failed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return None
-    result = 0
-    roman = {
+    """Converts a roman numeral to an integer."""
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return (0)
+
+    roman_dict = {
         "I": 1,
         "V": 5,
         "X": 10,
@@ -12,12 +16,16 @@ def roman_to_int(roman_string):
         "D": 500,
         "M": 1000
     }
+    num = 0
+
     for i in range(len(roman_string)):
-        if roman.get(roman_string[i], 0) == 0:
+        if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
+
         if (i != (len(roman_string) - 1) and
-                roman[roman_string[i]] < roman[roman_string[i + 1]]):
-            result += roman[roman_string[i]] * -1
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+            num += roman_dict[roman_string[i]] * -1
+
         else:
-            result += roman[roman_string[i]]
-    return result
+            num += roman_dict[roman_string[i]]
+    return (num)
